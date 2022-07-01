044c8bd8b1c02fbe0abfbd8f7c3ae5a5d22012d3
iteduki <mistolteen0127@gmail.com>
2022-07-02 06:52:45 +0900

    checkboxはerrorsにrefが含まれてしまうのでerrorの表示はやめる

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import type { DefaultValues, FieldValues } from 'react-hook-form'
 import { useFormContext } from 'react-hook-form'
 import { FormProvider, useForm } from 'react-hook-form'
@@ -29,18 +29,11 @@ export const Form = <T extends FieldValues = never>({
 }
 
 const InnerForm: React.FC = ({ children }) => {
-  const {
-    handleSubmit,
-    formState: { errors },
-  } = useFormContext()
+  const { handleSubmit } = useFormContext()
   const [result, setResult] = useState('')
-  const [error, setError] = useState('')
   const submit = handleSubmit((e) => {
     setResult(JSON.stringify(e))
   })
-  useEffect(() => {
-    setError(JSON.stringify(errors))
-  }, [errors, setError])
   return (
     <>
       <form method="post" onSubmit={submit}>
@@ -48,9 +41,6 @@ const InnerForm: React.FC = ({ children }) => {
       </form>
       <div>
         <textarea readOnly value={result} />
-      </div>
-      <div>
-        <textarea readOnly value={error} />
       </div>
     </>
   )
